bd7c68479024141b0eb097a96ccde783248bba48
ZwhsmD <z.dabrowskad@gmail.com>
2025-08-20 18:23:49 +0200

    Added Balance and Reset

--- React/finance-app/src/App.js
+++ React/finance-app/src/App.js
@@ -4,6 +4,7 @@ import api from './api'
 //React code which handles the event like onClick etc. It can only return 1 element
 const App = () => {
     const [transactions, setTransactions] = useState([]);
+    const [record, setLastRecord] = useState([]);
     const [formData, setFormData] = useState ({
         amount: '',
         category: '',
@@ -11,6 +12,11 @@ const App = () => {
         is_income: false,
         date: ''
     });
+
+    const fetchLastRecord = async () => {
+        const response = await api.get("/last-record/");
+        setLastRecord(response.data);
+    };
 
     const fetchTransactions = async() => {
         const response = await api.get("/transactions/");
@@ -21,6 +27,11 @@ const App = () => {
     useEffect(()=> {
         fetchTransactions();
     }, []); //run once after the first render
+
+    //run sth after render
+    useEffect(()=> {
+        fetchLastRecord();
+    }, [] );
 
     const handleInputChange = (event) => {
         //                              if checkbox click so True/False -option 1, |  if it is a text/number then option 2
@@ -33,10 +44,21 @@ const App = () => {
         });
     };
 
+    const handleResetDB = async () => {
+      try {
+        const response = await api.post("/reset-db/");
+        fetchTransactions()
+        fetchLastRecord()
+      } catch (error) {
+        console.error("Error resetting DB:", error);
+      }
+    };
+
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         await api.post("/transactions/", formData);
         fetchTransactions()
+        fetchLastRecord();
 
         //returning to the basic input = empty strings
         setFormData({
@@ -60,57 +82,84 @@ const App = () => {
                 </div>
             </nav>
 
-            <div className="container">
-                <form onSubmit = {handleFormSubmit}>
-                    <div className="mb-3 mt-3">
-                        <label htmlFor='amount' className='form-label'>
-                            Amount
-                            <input type='text' className="form-control" id="amount" name="amount" onChange={handleInputChange} value={formData.amount}>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 col-md-6 bg-light p-5">
+                        <form onSubmit = {handleFormSubmit}>
+                            <div className="mb-3 mt-3">
+                                <label htmlFor='amount' className='form-label'>
+                                    Amount
+                                    <input type='text' className="form-control" id="amount" name="amount" onChange={handleInputChange} value={formData.amount}>
 
-                            </input>
-                        </label>
+                                    </input>
+                                </label>
+                            </div>
+
+                            <div className="mb-3 ">
+                                <label htmlFor='category' className='form-label'>
+                                    Category
+                                    <input type='text' className="form-control" id="category" name="category" onChange={handleInputChange} value={formData.category}>
+
+                                    </input>
+                                </label>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor='description' className='form-label'>
+                                    Description
+                                    <input type='text' className="form-control" id="description" name="description" onChange={handleInputChange} value={formData.description}>
+
+                                    </input>
+                                </label>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor='is_income' className='form-label'>
+                                    Is Income?
+                                    <input type='checkbox' id="is_income" name="is_income" onChange={handleInputChange} value={formData.is_income}>
+                                    </input>
+                                </label>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor='date' className='form-label'>
+                                    Date
+                                    <input type='text' className="form-control" id="date" name="date" onChange={handleInputChange} value={formData.date}>
+                                    </input>
+                                </label>
+                            </div>
+
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
+
+                        </form>
+
+                        <button type="submit" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#resetModal" onClick={handleResetDB}>Reset</button>
+
+                                <div className="modal fade" id="resetModal" tabindex="-1" aria-labelledby="resetLabel" aria-hidden="true">
+                                    <div className="modal-dialog">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className="modal-title" id="resetLabel">Reset</h5>
+                                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div className="modal-body">
+                                                Database has successfully been reset
+                                            </div>
+                                            <div className="modal-footer">
+                                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                     </div>
 
-                    <div className="mb-3 ">
-                        <label htmlFor='category' className='form-label'>
-                            Category
-                            <input type='text' className="form-control" id="category" name="category" onChange={handleInputChange} value={formData.category}>
-
-                            </input>
-                        </label>
+                    <div className="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center p-5 bg-light display-1">
+                        Account Balance:
+                        <div className="d-flex align-items-center"><br /> {record ? record.current_balance : "No record found"}</div>
                     </div>
-
-                    <div className="mb-3">
-                        <label htmlFor='description' className='form-label'>
-                            Description
-                            <input type='text' className="form-control" id="description" name="description" onChange={handleInputChange} value={formData.description}>
-
-                            </input>
-                        </label>
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor='is_income' className='form-label'>
-                            Is Income?
-                            <input type='checkbox' id="is_income" name="is_income" onChange={handleInputChange} value={formData.is_income}>
-                            </input>
-                        </label>
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor='date' className='form-label'>
-                            Date
-                            <input type='text' className="form-control" id="date" name="date" onChange={handleInputChange} value={formData.date}>
-                            </input>
-                        </label>
-                    </div>
-
-                    <button type="submit" className="btn btn-primary">
-                        Submit
-                    </button>
-
-                </form>
-
+                </div>
                 <table className="table table-striped table-bordered table-hover">
                    <thead>
                         <tr>
@@ -134,7 +183,6 @@ const App = () => {
                    </tbody>
                 </table>
             </div>
-
         </div>
     )
 }
